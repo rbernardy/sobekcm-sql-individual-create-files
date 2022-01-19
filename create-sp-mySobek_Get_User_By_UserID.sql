33255f6c2b1ab1f39aea1052a5d3306620e116a1
Richard Bernardy <rbernard@usf.edu>
2022-01-18 19:43:43 -0500

    Add files via upload

---
+++ create-sp-mySobek_Get_User_By_UserID.sql
@@ -0,0 +1,104 @@
+USE [sobektest]
+GO
+
+/****** Object:  StoredProcedure [dbo].[mySobek_Get_User_By_UserID]    Script Date: 1/18/2022 7:43:23 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[mySobek_Get_User_By_UserID]
+	@userid int
+AS
+BEGIN
+
+	-- No need to perform any locks here
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	-- Get the basic user information
+	select UserID, ShibbID=coalesce(ShibbID,''), UserName=coalesce(UserName,''), EmailAddress=coalesce(EmailAddress,''), 
+	  FirstName=coalesce(FirstName,''), LastName=coalesce(LastName,''), Note_Length, 
+	  Can_Make_Folders_Public, isTemporary_Password, sendEmailOnSubmission, Can_Submit_Items, 
+	  NickName=coalesce(NickName,''), Organization=coalesce(Organization, ''), College=coalesce(College,''),
+	  Department=coalesce(Department,''), Unit=coalesce(Unit,''), Rights=coalesce(Default_Rights,''), Language=coalesce(UI_Language, ''), 
+	  Internal_User, OrganizationCode, EditTemplate, EditTemplateMarc, IsSystemAdmin, IsPortalAdmin, Include_Tracking_Standard_Forms,
+	  Descriptions=( select COUNT(*) from mySobek_User_Description_Tags T where T.UserID=U.UserID),
+	  Receive_Stats_Emails, Has_Item_Stats, Can_Delete_All_Items, ScanningTechnician, ProcessingTechnician, InternalNotes=coalesce(InternalNotes,''),
+	  IsHostAdmin, IsUserAdmin
+	from mySobek_User U
+	where ( UserID = @userid ) and ( isActive = 'true' );
+
+	-- Get the templates
+	select T.TemplateCode, T.TemplateName, GroupDefined='false', DefaultTemplate
+	from mySobek_Template T, mySobek_User_Template_Link L
+	where ( L.UserID = @userid ) and ( L.TemplateID = T.TemplateID )
+	union
+	select T.TemplateCode, T.TemplateName, GroupDefined='true', 'false'
+	from mySobek_Template T, mySobek_User_Group_Template_Link TL, mySobek_User_Group_Link GL
+	where ( GL.UserID = @userid ) and ( GL.UserGroupID = TL.UserGroupID ) and ( TL.TemplateID = T.TemplateID )
+	order by DefaultTemplate DESC, TemplateCode ASC;
+	
+	-- Get the default metadata
+	select P.MetadataCode, P.MetadataName, GroupDefined='false', CurrentlySelected
+	from mySobek_DefaultMetadata P, mySobek_User_DefaultMetadata_Link L
+	where ( L.UserID = @userid ) and ( L.DefaultMetadataID = P.DefaultMetadataID )
+	union
+	select P.MetadataCode, P.MetadataName, GroupDefined='true', 'false'
+	from mySobek_DefaultMetadata P, mySobek_User_Group_DefaultMetadata_Link PL, mySobek_User_Group_Link GL
+	where ( GL.UserID = @userid ) and ( GL.UserGroupID = PL.UserGroupID ) and ( PL.DefaultMetadataID = P.DefaultMetadataID )
+	order by CurrentlySelected DESC, MetadataCode ASC;
+
+	-- Get the bib id's of items submitted
+	select distinct( G.BibID )
+	from mySobek_User_Folder F, mySobek_User_Item B, SobekCM_Item I, SobekCM_Item_Group G
+	where ( F.UserID = @userid ) and ( B.UserFolderID = F.UserFolderID ) and ( F.FolderName = 'Submitted Items' ) and ( B.ItemID = I.ItemID ) and ( I.GroupID = G.GroupID );
+
+	-- Get the regular expression for editable items
+	select R.EditableRegex, GroupDefined='false', CanEditMetadata, CanEditBehaviors, CanPerformQc, CanUploadFiles, CanChangeVisibility, CanDelete
+	from mySobek_Editable_Regex R, mySobek_User_Editable_Link L
+	where ( L.UserID = @userid ) and ( L.EditableID = R.EditableID )
+	union
+	select R.EditableRegex, GroupDefined='true', CanEditMetadata, CanEditBehaviors, CanPerformQc, CanUploadFiles, CanChangeVisibility, CanDelete
+	from mySobek_Editable_Regex R, mySobek_User_Group_Editable_Link L, mySobek_User_Group_Link GL
+	where ( GL.UserID = @userid ) and ( GL.UserGroupID = L.UserGroupID ) and ( L.EditableID = R.EditableID );
+
+	-- Get the list of aggregations associated with this user
+	select A.Code, A.[Name], L.CanSelect, L.CanEditItems, L.IsAdmin AS IsAggregationAdmin, L.OnHomePage, L.IsCurator AS IsCollectionManager, GroupDefined='false', CanEditMetadata, CanEditBehaviors, CanPerformQc, CanUploadFiles, CanChangeVisibility, CanDelete
+	from SobekCM_Item_Aggregation A, mySobek_User_Edit_Aggregation L
+	where  ( L.AggregationID = A.AggregationID ) and ( L.UserID = @userid )
+	union
+	select A.Code, A.[Name], L.CanSelect, L.CanEditItems, L.IsAdmin AS IsAggregationAdmin, OnHomePage = 'false', L.IsCurator AS IsCollectionManager, GroupDefined='true', CanEditMetadata, CanEditBehaviors, CanPerformQc, CanUploadFiles, CanChangeVisibility, CanDelete
+	from SobekCM_Item_Aggregation A, mySobek_User_Group_Edit_Aggregation L, mySobek_User_Group_Link GL
+	where  ( L.AggregationID = A.AggregationID ) and ( GL.UserID = @userid ) and ( GL.UserGroupID = L.UserGroupID );
+
+	-- Return the names of all the folders
+	select F.FolderName, F.UserFolderID, ParentFolderID=isnull(F.ParentFolderID,-1), isPublic
+	from mySobek_User_Folder F
+	where ( F.UserID=@userid );
+
+	-- Get the list of all items associated with a user folder (other than submitted items)
+	select G.BibID, I.VID
+	from mySobek_User_Folder F, mySobek_User_Item B, SobekCM_Item I, SobekCM_Item_Group G
+	where ( F.UserID = @userid ) and ( B.UserFolderID = F.UserFolderID ) and ( F.FolderName != 'Submitted Items' ) and ( B.ItemID = I.ItemID ) and ( I.GroupID = G.GroupID );
+	
+	-- Get the list of all user groups associated with this user
+	select G.GroupName, Can_Submit_Items, Internal_User, IsSystemAdmin, IsPortalAdmin, Include_Tracking_Standard_Forms, G.UserGroupID
+	from mySobek_User_Group G, mySobek_User_Group_Link L
+	where ( G.UserGroupID = L.UserGroupID )
+	  and ( L.UserID = @userid );
+	  
+	-- Get the user settings
+	select * from mySobek_User_Settings where UserID=@userid order by Setting_Key;
+	  
+	-- Update the user table to include this as the last activity
+	update mySobek_User
+	set LastActivity = getdate()
+	where UserID=@userid;
+END;
+
+GO
+
