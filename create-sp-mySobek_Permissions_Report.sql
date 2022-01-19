937813a9321c011723803fd1e13983b8247845fe
Richard Bernardy <rbernard@usf.edu>
2022-01-18 21:27:23 -0500

    Add files via upload

---
+++ create-sp-mySobek_Permissions_Report.sql
@@ -0,0 +1,42 @@
+USE [sobektest]
+GO
+
+/****** Object:  StoredProcedure [dbo].[mySobek_Permissions_Report]    Script Date: 1/18/2022 9:26:55 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[mySobek_Permissions_Report] as
+begin
+
+	-- Return the top-level permissions (non-aggregation specific)
+	select '' as GroupName, U.UserID, UserName, EmailAddress, FirstName, LastName, Nickname, DateCreated, LastActivity, isActive, 
+		case when e.UserID is null then 'false' else 'true' end as Can_Edit_All_Items,
+		Internal_User, Can_Delete_All_Items, IsPortalAdmin, IsSystemAdmin, IsHostAdmin, IsUserAdmin
+	from mySobek_User as U left outer join
+		 mySobek_User_Editable_Link as E on E.UserID = U.UserID and E.EditableID = 1 
+	where      ( IsSystemAdmin = 'true' )
+			or ( IsPortalAdmin = 'true' )
+			or ( Can_Delete_All_Items = 'true' )
+			or ( IsHostAdmin = 'true' )
+			or ( Internal_User = 'true' )
+	union
+	select G.GroupName, U.UserID, UserName, EmailAddress, FirstName, LastName, Nickname, DateCreated, LastActivity, isActive, 
+		case when e.UserGroupID is null then 'false' else 'true' end as Can_Edit_All_Items,
+		G.Internal_User, G.Can_Delete_All_Items, G.IsPortalAdmin, G.IsSystemAdmin, 'false', 'false'
+	from mySobek_User as U inner join
+		 mySobek_User_Group_Link as L on U.UserID = L.UserID inner join
+		 mySobek_User_Group as G on G.UserGroupID = L.UserGroupID left outer join
+		 mySobek_User_Group_Editable_Link as E on E.UserGroupID = G.UserGroupID and E.EditableID = 1 
+	where      ( G.IsSystemAdmin = 'true' )
+			or ( G.IsPortalAdmin = 'true' )
+			or ( G.Can_Delete_All_Items = 'true' )
+			or ( G.Internal_User = 'true' )
+	order by LastName ASC, FirstName ASC, GroupName ASC;
+end;
+
+GO
+
