89f3059acccdce03ff4daa69427066e8196ac178
Richard Bernardy <rbernard@usf.edu>
2022-02-12 19:49:20 -0500

    Add files via upload

---
+++ create-sp-SobekCM_Get_Item_Aggregation_AllCodes.sql
@@ -0,0 +1,34 @@
+USE [sobektest]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SobekCM_Get_Item_Aggregation_AllCodes]    Script Date: 2/12/2022 7:48:36 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[SobekCM_Get_Item_Aggregation_AllCodes]
+AS
+begin
+	
+	-- No need to perform any locks here
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	
+	-- First, get the aggregations
+	SELECT P.Code, P.[Type], P.Name, ShortName=coalesce(P.ShortName, P.Name), P.isActive, P.Hidden, P.AggregationID, 
+	       [Description]=coalesce(P.[Description],''), ThematicHeadingID=coalesce(T.ThematicHeadingID, -1 ),
+		   External_URL=coalesce(P.External_Link,''), P.DateAdded, P.LanguageVariants, T.ThemeName, 
+		   F.ShortName as ParentShortName, F.Name as ParentName, F.Code as ParentCode
+	FROM SobekCM_Item_Aggregation AS P left outer join
+	     SobekCM_Thematic_Heading as T on P.ThematicHeadingID=T.ThematicHeadingID left outer join
+		 SobekCM_Item_Aggregation_Hierarchy as H on H.ChildID=P.AggregationID left outer join
+		 SobekCM_Item_Aggregation as F on F.AggregationID=H.ParentID
+	WHERE P.Deleted = 'false'
+	  and ( F.Deleted = 'false' or F.Deleted is null )
+	order by P.Code;
+
+end;
+GO
+
