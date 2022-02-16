c6ed8511c46601f5040955e1b4d8d0822d90b437
Richard Bernardy <rbernard@usf.edu>
2022-02-15 23:13:32 -0500

    Add files via upload

---
+++ create-sp-SobekCM_Get_Item_Statistics.sql
@@ -0,0 +1,26 @@
+USE [sobektest]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SobekCM_Get_Item_Restrictions]    Script Date: 2/15/2022 11:13:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[SobekCM_Get_Item_Restrictions]
+	@bibid varchar(10),
+	@vid varchar(5)
+AS
+BEGIN
+	select IP_Restriction_Mask, Dark
+	from SObekCM_Item I, SobekCM_Item_Group G
+	where ( I.VID = @vid )
+	  and ( I.GroupID = G.GroupID )
+	  and ( G.BibID=@bibid)
+	  and ( I.Deleted = 'false' )
+	  and ( G.Deleted = 'false' );
+END
+GO
+
